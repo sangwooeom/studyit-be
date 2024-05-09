1b7862ec3b381d64774159d1b71ef27ff4ea087d
sangwooeom <cdm19720@gmail.com>
2024-05-10 06:28:21 +0900

    프로젝트 초기화

---
+++ src/main/java/com/studyit/backend/StudyitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studyit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyit/backend/StudyitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
